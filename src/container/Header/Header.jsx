c978b390bb6729d1507664f86371a9f11586007f
AAli02 <aali.02p@gmail.com>
2022-12-02 10:00:25 -0500

    headerimg

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import person from '../../assets/person.png';
 
 import './Header.css';
 
@@ -6,8 +7,16 @@ const Header = () => (
   <div className="gpt3__header section__padding" id="home">
     <div className="gpt3__header-content">
       <h1 className="text__name">Murat Zay<span className='yellow__text'>.</span> </h1>
+      <div className='header__img'>
+        <img src={person} alt="" />
+      </div>
+
+      <div className='links__text'>
+
       <p className='socials'>Instagram | Linkdin</p>
       <p className='socials'>Resume | Social</p>
+
+      </div>
 
       <button id="contact__submit" class="contact__btn">
                 Contact
