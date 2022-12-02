8317b93aa33140d1124c5f7efa5f0bf69f0915a7
AAli02 <aali.02p@gmail.com>
2022-12-02 08:45:39 -0500

    nav done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
 
-import './Navbar.css';
+import "./Navbar.css";
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav>
+    <figure>
+      <img id="personal-logo" src="" alt="" />
+    </figure>
+    <ul className="nav__link--list">
+      <li className="nav__link">
+        <a href="#properties"
+          className="nav__link--anchor link__hover-effect">
+        Properties
+        </a>
+      </li>
+      <li className="nav__link">
+        <a
+          href="#properties"
+          className="nav__link--anchor link__hover-effect">
+        About
+        </a>
+      </li>
+      <li className="nav__link">
+        <a
+          href="#properties"
+          className="nav__link--anchor link__hover-effect">
+        Contact
+        </a>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -3,8 +3,15 @@ import React from 'react';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header-content">
+      <h1 className="gradient__text">Murat Zay</h1>
+      <p>Lorem, ipsum dolor.</p>
+
+      <button id="contact__submit" class="contact__btn">
+                Contact
+              </button>
+    </div>
   </div>
 );
 
